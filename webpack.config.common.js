9d63116487371789a8bbd4ca2aec39f37789fcf6
Tiago Carvalho <tcarvalho@chanzuckerberg.com>
2018-10-05 09:56:37 -0700

    Refactoring webpack for better compatibility

---
+++ webpack.config.common.js
@@ -0,0 +1,81 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const config = {
+  entry: `${path.resolve(__dirname, "app/assets/src/")}/index.js`,
+  output: {
+    path: path.resolve(__dirname, "app/assets/dist"),
+    filename: "bundle.min.js"
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundle.min.css"
+    })
+  ],
+  devtool: "source-map",
+  target: "web",
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        // exclude: path.resolve(__dirname, "node_modules/"),
+        loader: "babel-loader"
+      },
+      {
+        test: /.jsx$/,
+        // exclude: path.resolve(__dirname, "node_modules/"),
+        loader: "babel-loader"
+      },
+      {
+        // sass / scss loader for webpack
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              minimize: true,
+              sourceMap: true
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              minimize: true,
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|eot|ttf|svg)$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "fonts/[name].[ext]",
+          mimetype: "application/font-woff",
+          publicPath: url => {
+            return `/assets/${url.replace(/fonts/, "")}`;
+          }
+        }
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "fonts/[name].[ext]",
+          mimetype: "application/font-woff",
+          publicPath: url => {
+            return `/assets/${url.replace(/fonts/, "")}`;
+          }
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,18 +1,24 @@
 const merge = require("webpack-merge");
+const path = require("path");
 const webpack = require("webpack");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const devConfig = require("./webpack.config.dev.js");
+// const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const commonConfig = require("./webpack.config.common.js");
 
-module.exports = merge(devConfig, {
+module.exports = merge(commonConfig, {
   devtool: "source-map",
-  plugins: [
-    new UglifyJsPlugin({
-      uglifyOptions: {
-        mangle: false
-      }
-    }),
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("production")
-    })
-  ]
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "app/assets/dist"),
+    filename: "bundle.prod.min.js"
+  }
+  // plugins: [
+  //   new UglifyJsPlugin({
+  //     uglifyOptions: {
+  //       mangle: false
+  //     }
+  //   }),
+  //   new webpack.DefinePlugin({
+  //     "process.env.NODE_ENV": JSON.stringify("production")
+  //   })
+  // ]
 });
